b45b7c1e87b40d09e07124996ce15ea8ae1fa8b6
exnaidnet <119971717+exnaidnet@users.noreply.github.com>
2023-07-05 14:13:12 -0500

    Update address.js

--- models/address.js
+++ models/address.js
@@ -2,8 +2,8 @@ const db = require('../config/config');
 
 const Address = {};
 
-
-Address.findByUser = (id_user) => {
+// Zona de Vmarkets
+Address.findByUserV = (id_user) => {
     const sql = `
     SELECT
         id,
@@ -13,7 +13,7 @@ Address.findByUser = (id_user) => {
         lat,
         lng
     FROM
-        address
+        addressv
     WHERE 
         id_user = $1
     `;
@@ -21,10 +21,100 @@ Address.findByUser = (id_user) => {
     return db.manyOrNone(sql, id_user);
 }
 
-Address.create = (address) => {
+Address.createV = (address) => {
     const sql = `
     INSERT INTO
-        address(
+        addressv(
+            id_user,
+            address,
+            neighborhood,
+            lat,
+            lng,
+            created_at,
+            updated_at
+        )
+    VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id
+    `;
+
+    return db.oneOrNone(sql, [
+        address.id_user,
+        address.address,
+        address.neighborhood,
+        address.lat,
+        address.lng,
+        new Date(),
+        new Date()
+    ]);
+}
+
+// Zona de Servibambi
+Address.findByUserS = (id_user) => {
+    const sql = `
+    SELECT
+        id,
+        id_user,
+        address,
+        neighborhood,
+        lat,
+        lng
+    FROM
+        addresss
+    WHERE 
+        id_user = $1
+    `;
+
+    return db.manyOrNone(sql, id_user);
+}
+
+Address.createS = (address) => {
+    const sql = `
+    INSERT INTO
+        addresss(
+            id_user,
+            address,
+            neighborhood,
+            lat,
+            lng,
+            created_at,
+            updated_at
+        )
+    VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id
+    `;
+
+    return db.oneOrNone(sql, [
+        address.id_user,
+        address.address,
+        address.neighborhood,
+        address.lat,
+        address.lng,
+        new Date(),
+        new Date()
+    ]);
+}
+
+// Zona de Alorashop
+Address.findByUserA = (id_user) => {
+    const sql = `
+    SELECT
+        id,
+        id_user,
+        address,
+        neighborhood,
+        lat,
+        lng
+    FROM
+        addressa
+    WHERE 
+        id_user = $1
+    `;
+
+    return db.manyOrNone(sql, id_user);
+}
+
+Address.createA = (address) => {
+    const sql = `
+    INSERT INTO
+        addressa(
             id_user,
             address,
             neighborhood,
